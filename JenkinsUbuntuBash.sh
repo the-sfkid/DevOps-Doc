31044358d7d836547c0ba3d875f8a1da4ad9a3fe
Jithin Jose <152673164+the-sfkid@users.noreply.github.com>
2024-01-18 19:42:42 +0530

    Add files via upload

---
+++ JenkinsUbuntuBash.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+sudo apt update
+sudo apt install wget unzip -y
+
+sudo apt install default-jre -y
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+
+sudo systemctl start jenkins.service
+sudo systemctl status jenkins
+
+echo "InitialAdminPassword"
+echo "-------------------"
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword > adminpassword
