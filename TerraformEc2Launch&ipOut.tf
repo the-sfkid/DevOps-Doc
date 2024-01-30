f7e8c466a69f8152639dbc070b228cf1b3f3f37e
Jithin Jose <152673164+the-sfkid@users.noreply.github.com>
2024-01-30 12:50:52 +0530

    Add files via upload

---
+++ TerraformEc2Launch&ipOut.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "SF terraform"
+  }
+}
+
+output "instance_public_ip" {
+  description = "public ip of the server"
+  value       = aws_instance.app_server.public_ip
+}
